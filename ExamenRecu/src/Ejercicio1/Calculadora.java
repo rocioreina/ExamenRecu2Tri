7f6f84029e29eb83fbe2a82e4b24133bd2385d84
Rocío Reina Delgado <rocioreinadlg@gmail.com>
2020-05-18 17:07:11 +0200

    17:07

---
+++ ExamenRecu/src/Ejercicio1/Calculadora.java
@@ -0,0 +1,15 @@
+package Ejercicio1;
+
+import javax.swing.*;
+
+public class Calculadora {
+    private JTextField txtnum1;
+    private JTextField txtnum2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JLabel num1;
+    private JLabel num2;
+    private JTable table1;
+}
